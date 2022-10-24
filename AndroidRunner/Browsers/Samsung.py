76a8bc16eb8436a8853d8759158f7fec985e966d
pieterhop <pieterhoppenbrouwers@hotmail.com>
2022-10-25 00:08:14 +0200

    Bla

--- AndroidRunner/Browsers/Samsung.py
+++ AndroidRunner/Browsers/Samsung.py
@@ -4,4 +4,4 @@ from .Browser import Browser
 class Samsung(Browser):
     def __init__(self):
         # https://stackoverflow.com/a/28151563
-        super(Samsung, self).__init__('com.sec.android.app.sbrowser', 'com.sec.android.app.sbrowser/.SBrowserMainActivity')
+        super(Samsung, self).__init__('com.sec.android.app.sbrowser', 'com.sec.android.app.sbrowser.SBrowserMainActivity')
